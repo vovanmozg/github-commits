11188e35bd0bdde86cdbb05690e52ab0df241144
Vladimir Polukhin <vovanmozg@gmail.com>
2018-06-20 10:01:15 +0300

    add bulk download

--- grab.rb
+++ grab.rb
@@ -1,13 +1,27 @@
 require 'json'
 require 'rest-client'
 
-repos = RestClient.get('https://api.github.com/repositories')
-repos = JSON.parse(repos)
+def get_page
+	last_id = IO.read('data/last_id.txt')
 
-repos.each do |repo| 
-	cmd = "./get-repo-commits.sh #{repo['html_url']}"
-	`#{cmd}`
-	File.open("data/commits/#{repo['full_name'].gsub('/','-')}.json", 'w') { |f| f.write(JSON.pretty_generate(repo)) }
-	
+	repos = RestClient.get("https://api.github.com/repositories?since=#{last_id}")
+	repos = JSON.parse(repos)
+
+	repos.each do |repo| 
+		cmd = "./get-repo-commits.sh #{repo['html_url']}"
+		code = repo['full_name'].gsub('/','-')
+		json_file = "data/commits/#{code}.json"
+
+		if File.exist? json_file
+			p "skip #{repo['html_url']}"
+		else
+			`#{cmd}`
+			File.open(json_file, 'w') { |f| f.write(JSON.pretty_generate(repo)) }
+		end
+		IO.write('data/last_id.txt', repo['id'])
+	end
 end
 
+while(true) do
+	get_page	
+end
